1529f7883b9da2ce8a215a10e18dcff7c93e3d8a
Nicole <nicoleballet23@gmail.com>
2025-11-25 21:30:17 -0300

    Rota STATUS adicionada para teste de CI/CD

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Aplicação Online 🚀" });
 });
 
+// 🔥 Rota que valida se o deploy automático funcionou
 app.get("/status", (req, res) => {
   res.status(200).json({ message: "DEPLOY TESTE – SUCESSO!" });
 });
