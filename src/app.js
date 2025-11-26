37c5680ddfb2b909035c655556d91bc958e2869b
Nicole <nicoleballet23@gmail.com>
2025-11-25 21:26:45 -0300

    Corrigido app.js - deploy

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-app.get("/status", (req, res) => {
-  res.status(200).json({ message: "DEPLOY TESTE - SUCESSO!" });
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Aplicação Online 🚀" });
 });
 
+app.get("/status", (req, res) => {
+  res.status(200).json({ message: "DEPLOY TESTE – SUCESSO!" });
+});
+
+module.exports = app;
